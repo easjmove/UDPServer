3b55db0b14a34b61559739435d6573a7e7e856b0
move <move@zealand.dk>
2021-10-26 14:31:13 +0200

    Add project files.

---
+++ UDPClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("UDP Client");
+            byte[] data = Encoding.UTF8.GetBytes("hej med jer alle");
+            using (UdpClient socket = new UdpClient())
+            {
+                socket.Send(data, data.Length, "255.255.255.255", 5005);
+            }
+        }
+    }
+}
---
+++ UDPServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("UDP server!");
+
+            using (UdpClient socket = new UdpClient())
+            {
+                socket.Client.Bind(new IPEndPoint(IPAddress.Any, 5005));
+
+                while (true)
+                {
+                    IPEndPoint clientEndpoint = null;
+                    byte[] received = socket.Receive(ref clientEndpoint);
+                    Console.WriteLine("Client connected: " + clientEndpoint.Address);
+                    Console.WriteLine("Data: " + Encoding.UTF8.GetString(received));
+                }
+            }
+        }
+    }
+}
